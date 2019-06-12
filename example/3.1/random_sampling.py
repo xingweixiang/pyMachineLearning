493bff0f7307ad278d44e5bbd4f2e3be691a1e71
Administrator <281349503@qq.com>
2019-06-12 21:23:28 +0800

    随机采样

---
+++ example/3.1/random_sampling.py
@@ -0,0 +1,19 @@
+import random
+#随机采样dataMat数据集，number采样数
+def RandomSampling(dataMat,number): #无放回采样
+    try:  
+         slice = random.sample(dataMat, number)      
+         return slice  
+    except:  
+         print ('sample larger than population')
+  
+def RepetitionRandomSampling(dataMat,number):#放回采样
+    sample=[]  
+    for i in range(number):  
+         sample.append(dataMat[random.randint(0,len(dataMat)-1)])  
+    return sample
+
+if __name__ == '__main__':
+    dataMat = list(range(10))
+    print(RandomSampling(dataMat,3))#无放回采样
+    print(RepetitionRandomSampling(dataMat, 3))  # 无放回采样
